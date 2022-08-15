9de81822d6b8efd77a42719bb8a8d5b70015bbb8
Gabriel Miklós <gabriel.miklos@getbridge.com>
2022-08-15 15:08:49 +0200

    Clean up some unused dependencies.

--- buildSrc/src/main/kotlin/com/ouroboros/dependencies.kt
+++ buildSrc/src/main/kotlin/com/ouroboros/dependencies.kt
@@ -11,7 +11,6 @@ object Libs {
         const val version = "1.7.0"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
-        const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
     }
 
     object Coroutines {
@@ -24,22 +23,14 @@ object Libs {
     object AndroidX {
         const val appcompat = "androidx.appcompat:appcompat:1.3.1"
 
-        const val core = "androidx.core:core:1.6.0"
-        const val coreKtx = "androidx.core:core-ktx:1.6.0"
-        const val activityKtx = "androidx.activity:activity-ktx:1.3.1"
         const val activityCompose = "androidx.activity:activity-compose:1.3.1"
-        const val fragment = "androidx.fragment:fragment:1.3.6"
-        const val fragmentKtx = "androidx.fragment:fragment-ktx:1.3.6"
-        const val navigationUi = "androidx.navigation:navigation-ui:2.3.5"
+        const val navigationCompose = "androidx.navigation:navigation-compose:2.3.5"
 
         object Compose {
             const val version = "1.2.0"
 
             const val runtime = "androidx.compose.runtime:runtime:$version"
-            const val liveDataRuntime = "androidx.compose.runtime:runtime-livedata:$version"
             const val foundation = "androidx.compose.foundation:foundation:${version}"
-            const val layout = "androidx.compose.foundation:foundation-layout:${version}"
-
             const val ui = "androidx.compose.ui:ui:${version}"
             const val uiTooling = "androidx.compose.ui:ui-tooling:${version}"
             const val material = "androidx.compose.material:material:${version}"
@@ -55,8 +46,6 @@ object Libs {
                 private const val version = "1.1.2"
                 const val junit = "androidx.test.ext:junit-ktx:$version"
             }
-
-            const val espressoCore = "androidx.test.espresso:espresso-core:3.4.0"
         }
 
         object Lifecycle {
@@ -65,10 +54,5 @@ object Libs {
             const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
             const val viewmodelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:2.4.0"
         }
-    }
-
-    object Accompanist {
-        private const val version = "0.23.1"
-        const val insets = "com.google.accompanist:accompanist-insets:$version"
     }
 }
