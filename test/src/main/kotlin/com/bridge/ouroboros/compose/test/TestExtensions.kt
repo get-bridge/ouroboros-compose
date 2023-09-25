e363a89a4408d11765c097875911798da94f04e3
Daniel Voko <15357860+vokod@users.noreply.github.com>
2023-09-25 20:46:42 +0200

    gradle update and testing enhancements

--- test/src/main/kotlin/com/bridge/ouroboros/compose/test/TestExtensions.kt
+++ test/src/main/kotlin/com/bridge/ouroboros/compose/test/TestExtensions.kt
@@ -4,7 +4,8 @@ import com.bridge.ouroboros.compose.ActionableEvent
 import com.bridge.ouroboros.compose.ExecutableEffect
 import com.bridge.ouroboros.compose.Next
 import kotlinx.coroutines.runBlocking
-
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.coroutineContext
 
 infix fun <M, E : ActionableEvent<M, F>, F> M.receives(event: E) =
     event.perform(this)
@@ -19,10 +20,20 @@ infix fun <F : ExecutableEffect<E, S>, E, S> F.runWith(state: S): EffectResult<E
     return EffectResult(eventConsumer.items)
 }
 
+fun <F : ExecutableEffect<E, S>, E, S> F.runWith(
+    state: S,
+    context: CoroutineContext
+): EffectResult<E> {
+    val eventConsumer = TestConsumer<E>()
+
+    runInContext(context, state, eventConsumer)
+
+    return EffectResult(eventConsumer.items)
+}
+
 infix fun <M, F> Next<M, F>.matches(block: NextMatching<M, F>.() -> Unit) =
     NextMatching(this)(block)
 
 infix fun <E> EffectResult<E>.matches(block: EffectMatching<E>.() -> Unit) {
     EffectMatching(events)(block)
 }
-
