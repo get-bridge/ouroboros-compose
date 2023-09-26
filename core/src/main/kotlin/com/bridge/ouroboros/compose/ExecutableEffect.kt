d32cfa5a2332df1d0819e7bfb7efa42ec2d51521
Daniel Voko <15357860+vokod@users.noreply.github.com>
2023-09-26 12:54:16 +0200

    Testing enhancements (#2)

--- core/src/main/kotlin/com/bridge/ouroboros/compose/ExecutableEffect.kt
+++ core/src/main/kotlin/com/bridge/ouroboros/compose/ExecutableEffect.kt
@@ -1,13 +1,22 @@
 package com.bridge.ouroboros.compose
 
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlin.coroutines.CoroutineContext
 
 abstract class ExecutableEffect<EVENT, EFFECT_STATE> : CoroutineScope {
 
+    private val job = SupervisorJob()
+
     private var currentContext: CoroutineContext? = null
 
-    override val coroutineContext: CoroutineContext get() = checkNotNull(currentContext) { "coroutineContext must be attached before invoking perform" }
+    override val coroutineContext: CoroutineContext
+        get() {
+            checkNotNull(currentContext) { "coroutineContext must be attached before invoking perform" }
+            return job + defaultDispatcher
+        }
 
     abstract fun EFFECT_STATE.perform(emit: EventConsumer<EVENT>)
 
@@ -21,6 +30,15 @@ abstract class ExecutableEffect<EVENT, EFFECT_STATE> : CoroutineScope {
             state.perform(emit)
         } finally {
             currentContext = null
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var defaultDispatcher: CoroutineDispatcher = Dispatchers.Main
+
+        fun setDefaultDispatcher(dispatcher: CoroutineDispatcher) {
+            defaultDispatcher = dispatcher
         }
     }
 }
